688caafd17dd11dbbd9587ab0f4af8b6cf1f8a6a
Timothy Oliver <oliverwakogi@live.com>
2019-11-27 08:40:04 +0300

    complete login ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  showPassword = false
+  sendingRequest = false
+  loginForm: FormGroup
 
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
   }
 
+  private f(): any {return this.loginForm.controls}
 }
